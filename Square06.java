70dc7c2a76fa369200f530a0811ef4d85dc80c4d
auliaaa08 <ab.auliaabilqis@gmail.com>
2025-10-30 08:09:27 +0700

    Modifikasi for

--- Square06.java
+++ Square06.java
@@ -4,14 +4,13 @@ public class Square06 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Masukkan nilai N = ");
-        int N = sc.nextInt();
-        for (int iOuter = 1; iOuter <= N; iOuter++) {
-            for (int i = 0; i <= N; i++) {
+        System.out.print("Masukkan nilai n = ");
+        int n = sc.nextInt();
+        for (int iOuter = 1; iOuter <= n; iOuter++) {
+            for (int i = 0; i <= n; i++) {
                 System.out.print("*");
             }
             System.out.println();
-            
         }
     }
 }
