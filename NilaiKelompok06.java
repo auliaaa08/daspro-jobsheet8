5a0c701a87d2b1e893f08ec93cc41e72ac160a20
auliaaa08 <ab.auliaabilqis@gmail.com>
2025-10-30 10:32:34 +0700

    NilaiKelompok setelah modifikasi

--- NilaiKelompok06.java
+++ NilaiKelompok06.java
@@ -1,27 +1,38 @@
 
 import java.util.Scanner;
+
 public class NilaiKelompok06 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int i, j, nilai;
-        float totalNilai, rataNilai;
+        int i, j, nilai, kelompokTertinggi = 0;;
+        float totalNilai, rataNilai, rataTertinggi = 0;
 
         i = 1;
         while (i <= 6) {
             System.out.println("Kelompok " + i);
+            totalNilai = 0;
+
+            for (j = 1; j <= 5; j++) {
+                System.out.print("Nilai dari Kelompok Penilai " + j + ": ");
+                nilai = sc.nextInt();
+                totalNilai += nilai;
+            }
+
+            rataNilai = totalNilai / 5;
+            System.out.println("Kelompok " + i + " : nilai rata-rata " + rataNilai);
+            System.out.println();
+
+            if (rataNilai > rataTertinggi) {
+                rataTertinggi = rataNilai;
+                kelompokTertinggi = i;
+            }
             i++;
         }
 
-        totalNilai = 0;
-        for (j = 1; j <= 5; j++) {
-            System.out.print(" Nilai dari Kelompok Penilai " + j + ": ");
-            nilai = sc.nextInt();
-            totalNilai += nilai;
-        }
-
-        rataNilai = totalNilai / 5;
-        System.out.println("Rata-rata nilai = " + rataNilai);
+        System.out.println("Kelompok dengan nilai rata-rata tertinggi: " + kelompokTertinggi);
+        System.out.println("Kelompok " + kelompokTertinggi + " dengan nilai rata-rata = " + rataTertinggi);
+        
     }
 }
